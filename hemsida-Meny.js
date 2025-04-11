a2ee57137ce680678436baa009c8367f3ea94b34
Pelle Wåhlen <pelle.wahlen@skola.tby.se>
2025-04-11 15:24:29 +0200

    Knappar och lite java

---
+++ hemsida-Meny.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const startText = document.getElementById("menyText");
+    
+    startText.classList.add("fade-in");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const dropdown = document.querySelector(".dropdown");
+    const dropdownContent = document.querySelector(".dropdown-content");
+
+    dropdown.addEventListener("mouseenter", () => {
+        dropdownContent.style.maxHeight = "200px";
+        dropdownContent.style.opacity = "1";
+    });
+
+    dropdown.addEventListener("mouseleave", () => {
+        dropdownContent.style.maxHeight = "0";
+        dropdownContent.style.opacity = "0";
+    });
+});
