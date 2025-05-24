df005f6d2b18e588f457ea81fec1c91bbb497a29
Pelle Wåhlen <pelle.wahlen@skola.tby.se>
2025-05-25 01:06:31 +0200

    Lagt till kontaktformulär, fixat rubriker och annat + uppdaterat meny-flikar

--- KONTAKT SIDA/hemsida-kontakt.js
+++ KONTAKT SIDA/hemsida-kontakt.js
@@ -18,3 +18,19 @@ document.addEventListener("DOMContentLoaded", () => {
         dropdownContent.style.opacity = "0";
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll('.scroll-fade');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    elements.forEach(el => observer.observe(el));
+});
